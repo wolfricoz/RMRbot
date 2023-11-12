5c5e79a6f0eadd0f380315939969938bc7185df3
wolfricoz <ricoisdenaam@gmail.com>
2023-11-13 00:59:53 +0100

    RMRbot 4.0.2: updates to the search ban section and minor bugfixes
    Signed-off-by: wolfricoz <ricoisdenaam@gmail.com>

--- modules/tasks.py
+++ modules/tasks.py
@@ -160,8 +160,12 @@ class Tasks(commands.GroupCog):
             for channel in x.channels:
                 if channel.type == discord.ChannelType.forum:
                     async for post in channel.archived_threads():
-                        message = await post.send(f"{post.owner.mention} Your advert has been unarchived. If this advert is no longer relevant, please close it with /forum close")
-                        await asyncio.sleep(1)
+                        try:
+                            await post.send(f"{post.owner.mention} Your advert has been unarchived. If this advert is no longer relevant, please close it with /forum close")
+                            await asyncio.sleep(1)
+                        except AttributeError:
+                            await post.send(f"Your advert has been unarchived. If this advert is no longer relevant, please close it with /forum close")
+                            await asyncio.sleep(1)
 
     @unarchiver.before_loop  # it's called before the actual task runs
     async def before_checkactiv(self):
