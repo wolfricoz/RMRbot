db7b981e7afda1e18265e2cddaf30aaab73b0ae8
wolfricoz <ricoisdenaam@gmail.com>
2024-01-29 05:04:05 +0100

    Bug fixes
    Signed-off-by: wolfricoz <ricoisdenaam@gmail.com>

--- classes/databaseController.py
+++ classes/databaseController.py
@@ -504,7 +504,7 @@ class SearchWarningTransactions(ABC):
         userdata = session.scalars(Select(Warnings).where(Warnings.uid == userid)).all()
         session.close()
         for x in userdata:
-            if x.entry > monthsago:
+            if x.entry < monthsago:
                 active += 1
             total += 1
         return total, active
