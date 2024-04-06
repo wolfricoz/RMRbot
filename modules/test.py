5aba4b9aa38bc4f17423aeb1559ded91cdede053
wolfricoz <ricoisdenaam@gmail.com>
2024-04-06 23:47:19 +0200

    updated the idlist message
    Signed-off-by: wolfricoz <ricoisdenaam@gmail.com>

--- modules/test.py
+++ modules/test.py
@@ -3,13 +3,11 @@ import datetime
 import json
 import os
 import re
-import time
 from time import perf_counter
+
 import discord
-from discord import Button
 from discord.ext import commands
 from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker
 
 from classes.databaseController import ConfigData
 
@@ -71,7 +69,6 @@ class Test(commands.Cog, name="test"):
                 await channel.delete(reason=f"empty command used by {ctx.author}")
         else:
             await ctx.send("Dev command.")
-
 
     @commands.command()
     @commands.is_owner()
@@ -162,13 +159,14 @@ class Test(commands.Cog, name="test"):
     @commands.command(name="cbans")
     @commands.is_owner()
     async def checkbans(self, ctx: commands.Context):
-        print("checking bans")
+        await ctx.send("checking bans")
         async for x in ctx.guild.bans():
             if x.reason is None:
                 continue
             match = re.match(r"(((0[0-9])|(1[012]))([\/|\-|.])((0[1-9])|([12][0-9])|(3[01]))([\/|\-|.])((20[012]\d|19\d\d)|(1\d|2[0123])))", x.reason)
             if match is not None:
                 await ctx.send(f"{x.user.mention} {x.reason}")
+        await ctx.send("done")
 
 
 async def setup(bot: commands.Bot):
