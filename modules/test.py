8a7ffcf30867c5bcb19e8c8e9e98ad0223b283cb
wolfricoz <ricoisdenaam@gmail.com>
2024-04-06 23:54:25 +0200

    updated the idlist message
    Signed-off-by: wolfricoz <ricoisdenaam@gmail.com>

--- modules/test.py
+++ modules/test.py
@@ -163,7 +163,7 @@ class Test(commands.Cog, name="test"):
         async for x in ctx.guild.bans():
             if x.reason is None:
                 continue
-            match = re.match(r"(((0[0-9])|(1[012]))([\/|\-|.])((0[1-9])|([12][0-9])|(3[01]))([\/|\-|.])((20[012]\d|19\d\d)|(1\d|2[0123])))", x.reason)
+            match = re.search(r"(((0[0-9])|(1[012]))([\/|\-|.])((0[1-9])|([12][0-9])|(3[01]))([\/|\-|.])((20[012]\d|19\d\d)|(1\d|2[0123])))", x.reason)
             if match is not None:
                 await ctx.send(f"{x.user.mention} {x.reason}")
         await ctx.send("done")
