9fe0ea3fb99aecd85c33fc2205550ffec51e41c3
wolfricoz <ricoisdenaam@gmail.com>
2024-05-18 19:20:42 +0200

    Added checking for invalid invites and log rotation
    Signed-off-by: wolfricoz <ricoisdenaam@gmail.com>

--- classes/lobbyprocess.py
+++ classes/lobbyprocess.py
@@ -112,8 +112,8 @@ class LobbyProcess(ABC):
         general = ConfigData().get_key(guild.id, "general")
         message = ConfigData().get_key(guild.id, "welcomemessage")
         channel = guild.get_channel(int(general))
-        async for message in channel.history(limit=20):
-            if message.author.bot and user in message.mentions:
+        async for cmessage in channel.history(limit=20):
+            if cmessage.author.bot and user in cmessage.mentions:
                 print("welcome message already sent")
                 return
         await channel.send(f"Welcome to {guild.name} {user.mention}! {message}")
