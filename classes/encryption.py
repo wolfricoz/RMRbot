30d4a7989390292d3872ae999007602feefc1f2f
wolfricoz <ricoisdenaam@gmail.com>
2025-09-01 12:56:36 +0200

    Encryption fix

--- classes/encryption.py
+++ classes/encryption.py
@@ -6,26 +6,21 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 EncryptionKey = os.getenv("KEY")
 
-
-class Encryption:
-    def __init__(self):
+class Encryption :
+    def __init__(self) :
         self.fernet = Fernet(EncryptionKey)
 
-    class Encryption :
-        def __init__(self) :
-            self.fernet = Fernet(EncryptionKey)
+    def encrypt(self, text: str) -> str :
+        """Encrypts the text"""
+        return str(self.fernet.encrypt(text.encode()))
 
-        def encrypt(self, text: str) -> str :
-            """Encrypts the text"""
-            return str(self.fernet.encrypt(text.encode()))
-
-        def decrypt(self, text: bytes | str) -> str :
-            """Decrypts the text"""
-            if text is None :
-                return "No Dob Stored"
-            if text.startswith("b'") and text.endswith("'") :
-                text = text[2 :-1]
-            if text.startswith('b"') and text.endswith('"') :
-                text = text[2 :-1]
-            return self.fernet.decrypt(text).decode()
+    def decrypt(self, text: bytes | str) -> str :
+        """Decrypts the text"""
+        if text is None :
+            return "No Dob Stored"
+        if text.startswith("b'") and text.endswith("'") :
+            text = text[2 :-1]
+        if text.startswith('b"') and text.endswith('"') :
+            text = text[2 :-1]
+        return self.fernet.decrypt(text).decode()
 
